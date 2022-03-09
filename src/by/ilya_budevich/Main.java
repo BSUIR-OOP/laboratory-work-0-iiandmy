1809b444b4d7bb2e97103de077d62b4ebcec79f6
iia.m.y <frezhgrand@gmail.com>
2022-03-09 23:34:17 +0300

    init commit

---
+++ src/by/ilya_budevich/Main.java
@@ -0,0 +1,7 @@
+package by.ilya_budevich;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/by/ilya_budevich/model/Figure.java
@@ -0,0 +1,5 @@
+package by.ilya_budevich.model;
+
+public abstract class Figure {
+
+}
